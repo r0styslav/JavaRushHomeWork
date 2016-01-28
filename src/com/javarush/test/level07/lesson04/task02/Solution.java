78bd50dd94a2d99ad816cf4d72db525c9738dbda
Rostyslav.Pash <avenger2010ua@gmail.com>
2016-01-28 19:43:20 +0200

    level07.lesson4.task2

--- src/com/javarush/test/level07/lesson04/task02/Solution.java
+++ src/com/javarush/test/level07/lesson04/task02/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level07.lesson04.task02;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.BufferOverflowException;
 
 /* Массив из строчек в обратном порядке
 1. Создать массив на 10 строчек.
@@ -14,6 +15,25 @@ public class Solution
     public static void main(String[] args) throws Exception
     {
         //напишите тут ваш код
+        String array[] = new String[10];
+        String arrayBack[] = new String[10];
+        int numberCount = 8;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < numberCount; i++)
+        {
+            array[i] = bufferedReader.readLine();
+        }
+
+        for (int i = 0; i < array.length; i++)
+        {
+            arrayBack[i] = array[array.length - i - 1];
+        }
+
+
+        for (String value : arrayBack)
+        {
+            System.out.println(value);
+        }
 
     }
 }
