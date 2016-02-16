fa4a45e73a1976e62d8f41f09dfe3d7c0946c600
Rostyslav.Pash <avenger2010ua@gmail.com>
2016-02-16 19:07:34 +0200

    level08.lesson08.tasks4-5

--- src/com/javarush/test/level08/lesson08/task04/Solution.java
+++ src/com/javarush/test/level08/lesson08/task04/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.test.level08.lesson08.task04;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /* Удалить всех людей, родившихся летом
 Создать словарь (Map<String, Date>) и занести в него десять записей по принципу: «фамилия» - «дата рождения».
@@ -39,14 +36,16 @@ public class Solution
     public static void removeAllSummerPeople(HashMap<String, Date> map)
     {
         //напишите тут ваш код
-        Map.Entry<String, Date> newMap;
         Iterator<Map.Entry<String, Date>> iter = map.entrySet().iterator();
         while (iter.hasNext())
         {
-            newMap = iter.next();
-            Date date = newMap.getValue();
-            if (date.getMonth() == 5 || date.getMonth() == 6 || date.getMonth() == 7)
-                map.remove(newMap.getKey());
+            //Date date = newMap.getValue();
+            Calendar date = Calendar.getInstance();
+            date.setTime(iter.next().getValue());
+            if (date.get(Calendar.MONTH) == Calendar.JUNE
+                    || date.get(Calendar.MONTH) == Calendar.JULY
+                    || date.get(Calendar.MONTH) == Calendar.AUGUST)
+                iter.remove();
         }
     }
 }
