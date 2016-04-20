dd92170da61d4131baa2ad8f3b80481a48abfd59
Rostyslav.Pash <avenger2010ua@gmail.com>
2016-04-20 19:18:01 +0300

    tasks lvl10.lesson11

--- src/com/javarush/test/level09/lesson11/bonus03/Solution.java
+++ src/com/javarush/test/level09/lesson11/bonus03/Solution.java
@@ -1,8 +1,14 @@
 package com.javarush.test.level09.lesson11.bonus03;
 
+import com.sun.org.apache.xerces.internal.impl.dv.xs.BooleanDV;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 /* Задача по алгоритмам
 Задача: Пользователь вводит с клавиатуры список слов (и чисел). Слова вывести в возрастающем порядке, числа - в убывающем.
@@ -50,8 +56,38 @@ public class Solution
 
     public static void sort(String[] array)
     {
-        //напишите тут ваш код
+        ArrayList<Integer> intArray = new ArrayList<>();
+        ArrayList<String> strArray = new ArrayList<>();
+        ArrayList<Integer> numberPosition = new ArrayList<>();
+        ArrayList<Integer> stringPosition = new ArrayList<>();
+        Boolean[] isString = new Boolean[array.length];
+        int k = 0, y = 0;
 
+        //напишите тут ваш код
+        for (int i = 0; i < array.length; i++) {
+            if (isNumber(array[i])) {
+                intArray.add(Integer.parseInt(array[i]));
+                isString[i] = false;
+            } else
+            {
+                strArray.add(array[i]);
+                isString[i] = true;
+            }
+        }
+
+        Collections.sort(intArray, Collections.<Integer>reverseOrder());
+        Collections.sort(strArray);
+
+        for (int i = 0; i < array.length; i++) {
+            if (isString[i]) {
+                array[i] = strArray.get(k++);
+            } else {
+                array[i] = intArray.get(y++).toString();
+            }
+        }
+
+        //System.out.println(intArray);
+        //System.out.println(strArray);
     }
 
     //Метод для сравнения строк: 'а' больше чем 'b'
