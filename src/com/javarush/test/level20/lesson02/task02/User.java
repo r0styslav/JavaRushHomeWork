62378f39c7d09dc9a293eb5105ec209fa59e1c8c
Rostyslav.Pash <avenger2010ua@gmail.com>
2016-08-11 18:45:27 +0300

    new

--- src/com/javarush/test/level20/lesson02/task02/User.java
+++ src/com/javarush/test/level20/lesson02/task02/User.java
@@ -65,4 +65,9 @@ public class User {
         }
     }
 
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " "
+                + birthDate + " " + isMale + " " + country;
+    }
 }
