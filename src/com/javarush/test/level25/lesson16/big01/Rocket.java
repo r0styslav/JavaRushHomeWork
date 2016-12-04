5ee21b539324b2fe01a2f828cdff01abd1b23803
rost_HOME <avenger2010ua@gmail.com>
2016-12-04 20:02:41 +0200

    lvl25.lesson16.big

--- src/com/javarush/test/level25/lesson16/big01/Rocket.java
+++ src/com/javarush/test/level25/lesson16/big01/Rocket.java
@@ -1,7 +1,31 @@
 package com.javarush.test.level25.lesson16.big01;
 
 /**
- * Created by Rost on 23-Sep-16.
+ * Класс для объектов-ракета
  */
-public class Rocket extends BaseObject {
+public class Rocket  extends BaseObject
+{
+
+    public Rocket(double x, double y)
+    {
+        super(x, y, 1);
+    }
+
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    @Override
+    public void draw(Canvas canvas)
+    {
+        canvas.setPoint(x,y,'R');
+    }
+
+    /**
+     * Двигаем себя вверх на один ход.
+     */
+    @Override
+    public void move()
+    {
+        y--;
+    }
 }
