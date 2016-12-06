2f25a7a11f005ea231c19653a45f6c6f8d8a8899
Rostyslav.Pash <avenger2010ua@gmail.com>
2016-12-06 17:46:29 +0200

    - new tasks done

---
+++ src/com/javarush/test/level26/lesson15/big01/CashMachine.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level26.lesson15.big01;
+
+/**
+ * Created by Rostyslav.Pash on 15.11.2016.
+ */
+public class CashMachine {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level26.lesson15.big01;
+
+/**
+ * Created by Rostyslav.Pash on 15.11.2016.
+ */
+public class ConsoleHelper {
+    public static void writeMessage(String message) {
+
+    }
+
+    
+}
